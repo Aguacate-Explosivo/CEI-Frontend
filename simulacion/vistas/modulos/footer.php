0d352a609e3d0fb38561eb1d32b1b40c3015a3ee
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-05-30 22:16:01 -0500

    actualizacion base de datos
    merge funcion de proyectos

---
+++ simulacion/vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2023 <a href="www.facnet.com" target="_blank">Facnet - Aguacate Explosivo dev Team</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
