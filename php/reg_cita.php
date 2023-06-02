15c7dffff8839c7546addbd5e450d3cb1332fbf2
phelipe009 <phelipecordoba009@gmail.com>
2023-06-02 11:26:12 -0500

    Update reg_cita.php

--- php/reg_cita.php
+++ php/reg_cita.php
@@ -1,29 +1,38 @@
 <?php
     //Conexion con la base de datos
-	include_once("../conexion.php");
-	$conexion=conectar();
+    include_once("../conexion.php");
+    $conexion = conectar();
 
     //Valores del formulario
-    $documento=$_POST['documento'];
-    $nombre=$_POST['nombre'];
-    $tel=$_POST['tel'];
-    $direccion=$_POST['direccion'];
-    $fecha=$_POST['fecha'];
+    $documento = $_POST['documento'];
+    $nombre = $_POST['nombre'];
+    $tel = $_POST['tel'];
+    $direccion = $_POST['direccion'];
+    $fecha = $_POST['fecha'];
+
+    // Verificar si existe una cita con la misma fecha y hora
+    $consultaExistente = "SELECT * FROM citas WHERE fecha_hora = '$fecha'";
+    $resultadoExistente = mysqli_query($conexion, $consultaExistente);
     
-    //se Obtiene la longitud del string
-    
-    //igresar la informacion a la tabla de datos
-   $consulta = "INSERT INTO citas VALUES ('','$documento','$nombre','$tel','$direccion','$fecha')";
-    $resultado=mysqli_query($conexion,$consulta);
-    if ($resultado) {
-      echo "<script language='JavaScript'>
-                   location.assign('../citas.php');
-                   </script>"; 
-          
-        }else{
-          echo "<script language='JavaScript'>
-          alert('ya hay una cita para esta fecha y hora ');
-             location.assign('../citas.php');
-             </script>"; 
+    if (mysqli_num_rows($resultadoExistente) > 0) {
+        echo "<script language='JavaScript'>
+                alert('Ya hay una cita para esta fecha y hora');
+                location.assign('../citas.php');
+              </script>";
+    } else {
+        // Insertar la información en la tabla de datos
+        $consulta = "INSERT INTO citas VALUES ('', '$documento', '$nombre', '$tel', '$direccion', '$fecha')";
+        $resultado = mysqli_query($conexion, $consulta);
+
+        if ($resultado) {
+            echo "<script language='JavaScript'>
+                    location.assign('../citas.php');
+                  </script>";
+        } else {
+            echo "<script language='JavaScript'>
+                    alert('Error al insertar la cita');
+                    location.assign('../citas.php');
+                  </script>";
         }
+    }
 ?>
