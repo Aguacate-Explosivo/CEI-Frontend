de0792fc450fdebbbf5d724fe2b2c077b937a936
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-06-02 13:57:54 -0500

    correcion: implementacion del sweet alert en las citas

--- php/reg_cita.php
+++ php/reg_cita.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <title>Procesando Cita</title>
+</head>
+<body>  
 <?php
     //Conexion con la base de datos
 	include_once("../conexion.php");
@@ -16,14 +26,29 @@
    $consulta = "INSERT INTO citas VALUES ('','$documento','$nombre','$tel','$direccion','$fecha')";
     $resultado=mysqli_query($conexion,$consulta);
     if ($resultado) {
-      echo "<script language='JavaScript'>
-                   location.assign('../citas.php');
-                   </script>"; 
+      echo "<script>
+            Swal.fire({
+              icon: 'success',
+              title: 'Registro Exitoso...',
+              text: 'Su Cita ha sido guardada de forma correcta',
+            })
+            setInterval(()=>{
+              location.assign('../citas.php');
+            },5000)
+            </script>"; 
           
         }else{
-          echo "<script language='JavaScript'>
-          alert('ya hay una cita para esta fecha y hora ');
-             location.assign('../citas.php');
+          echo "<script>
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'ya hay una cita para esta fecha y hora!',
+          })
+          setInterval(()=>{
+            location.assign('../citas.php');
+          },5000)
              </script>"; 
         }
 ?>
+</body>
+</html>
