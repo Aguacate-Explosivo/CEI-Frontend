d8f243b4fb272c589b0b2a43cbb6f06067264b93
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-06-07 13:36:28 -0500

    optimizacion cabecera

--- cabecera.php
+++ cabecera.php
@@ -1,43 +1,39 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
-    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Título de la página</title>
 
     <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+    <link rel="icon" href="img/favicon.ico">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="lib/animate/animate.min.css">
+    <link rel="stylesheet" href="lib/owlcarousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="lib/lightbox/css/lightbox.min.css">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <!-- Template Stylesheet -->
-    <link href="css\style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-
-<body  >  
+<body>
     <!-- Spinner Start -->
-    <div id="spinner"
-        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;"></div>
     </div>
     <!-- Spinner End -->
-
 
     <!-- Topbar Start -->
     <div class="container-fluid bg-primary text-white d-none d-lg-flex">
@@ -51,19 +47,15 @@
                     <small class="ms-4"><i class="fa fa-envelope me-3"></i> cei@utch.edu.co</small>
                     <small class="ms-4"><i class="fa fa-phone-alt me-3"></i>+57 314 509 3025</small>
                     <div class="ms-3 d-flex">
-                        <a class="btn btn-sm-square btn-light text-success rounded-circle ms-2" href=""><i
-                                class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-sm-square btn-light text-success rounded-circle ms-2" href=""><i
-                                class="fab fa-twitter"></i></a>
-                        <a class="btn btn-sm-square btn-light text-success rounded-circle ms-2" href=""><i
-                                class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-sm-square btn-light text-success rounded-circle ms-2" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-sm-square btn-light text-success rounded-circle ms-2" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-sm-square btn-light text-success rounded-circle ms-2" href=""><i class="fab fa-linkedin-in"></i></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- Topbar End -->
-
 
     <!-- Navbar Start -->
     <div class="container-fluid bg-white sticky-top">
@@ -72,22 +64,20 @@
                 <a href="index.php" class="navbar-brand d-lg-none">
                     <h1 class="fw-bold m-0"></h1>
                 </a>
-                <button type="button"   class="navbar-toggler me-0" data-bs-toggle="collapse"
-                    data-bs-target="#navbarCollapse">
+                <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav">
-                        <a href="index.php" class="nav-item nav-link ">Inicio</a>
+                        <a href="index.php" class="nav-item nav-link">Inicio</a>
                         <a href="citas.php" class="nav-item nav-link">Agendar Citas</a>
                         <a href="eventos.php" class="nav-item nav-link">Galeria De Eventos</a>
                         <a href="cursos.php" class="nav-item nav-link">Cursos</a>
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Otros</a>
                             <div class="dropdown-menu bg-light rounded-0 rounded-bottom m-0">
-                            <a href="nosotros.php" class="dropdown-item">Nosotros</a>
-                            <a href="servicios.php" class="dropdown-item">Servicios</a>
-
+                                <a href="nosotros.php" class="dropdown-item">Nosotros</a>
+                                <a href="servicios.php" class="dropdown-item">Servicios</a>
                                 <a href="#" class="dropdown-item">Descargar Formatos</a>
                                 <!-- <a href="#" class="dropdown-item">Quotation</a> -->
                                 <!-- <a href="#" class="dropdown-item">404 Page</a> -->
@@ -96,19 +86,11 @@
                         <a href="contacto.php" class="nav-item nav-link">Contacto</a>
                         <a href="login.php" class="nav-item nav-link">Login</a>
                     </div>
-                    
                 </div>
             </nav>
         </div>
     </div>
     <!-- Navbar End -->
-
-
-    
-
-
-    
-
 
     <!-- JavaScript Libraries -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
@@ -122,5 +104,4 @@
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
 </body>
-
 </html>
