455bcb96e2de6bab8d440be9058ccf992b6f4b73
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-06-07 17:12:40 -0500

    optimización de footer

--- footer.php
+++ footer.php
@@ -1,70 +1,61 @@
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-4">Contacto</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Cra. 22 No 18B-10 B/ Nicolás Medrano - Ciudadela Universitaria.
-                        Bloque 8, salón 316-318 </p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+57 314 509 3025</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i> cei@utch.edu.co</p>
-                    <div class="d-flex pt-3">
-                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i
-                                class="fab fa-twitter"></i></a>
-                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i
-                                class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i
-                                class="fab fa-youtube"></i></a>
-                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i
-                                class="fab fa-linkedin-in"></i></a>
-                    </div>
+<!-- Footer Start -->
+<div class="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-white mb-4">Contacto</h4>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Cra. 22 No 18B-10 B/ Nicolás Medrano - Ciudadela Universitaria. Bloque 8, salón 316-318</p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+57 314 509 3025</p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>cei@utch.edu.co</p>
+                <div class="d-flex pt-3">
+                    <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-linkedin-in"></i></a>
                 </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-4">Enlaces Rapidos</h4>
-                    <a class="btn btn-link" href="index.php">Inicio</a>
-                    <a class="btn btn-link" href="contacto.php">Contacto</a>
-                    <a class="btn btn-link" href="servicios.php">Servicios</a>
-                    <a class="btn btn-link" href="cursos.php">Cursos</a>
-                    <a class="btn btn-link" href="nosotros.php">Nosotros</a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-4">Horario De Atencion</h4>
-                    <p class="mb-1">Lunes - Viernes</p>
-                    <h6 class="text-light">09:00 am - 05:00 pm</h6>
-                    <p class="mb-1">Sabados</p>
-                    <h6 class="text-light">09:00 am - 12:00 pm</h6>
-                    <p class="mb-1">Domingo</p>
-                    <h6 class="text-light">Cerrado</h6>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-4">Informaciones</h4>
-                    <p>Registra tu correo para recibir informacion  </p>
-                    <div class="position-relative w-100">
-                        <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text"
-                            placeholder="Correo">
-                        <button type="button"
-                            class="btn btn-light py-2 position-absolute top-0 end-0 mt-2 me-2">Enviar</button>
-                    </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-white mb-4">Enlaces Rápidos</h4>
+                <a class="btn btn-link" href="index.php">Inicio</a>
+                <a class="btn btn-link" href="contacto.php">Contacto</a>
+                <a class="btn btn-link" href="servicios.php">Servicios</a>
+                <a class="btn btn-link" href="cursos.php">Cursos</a>
+                <a class="btn btn-link" href="nosotros.php">Nosotros</a>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-white mb-4">Horario De Atención</h4>
+                <p class="mb-1">Lunes - Viernes</p>
+                <h6 class="text-light">09:00 am - 05:00 pm</h6>
+                <p class="mb-1">Sábados</p>
+                <h6 class="text-light">09:00 am - 12:00 pm</h6>
+                <p class="mb-1">Domingo</p>
+                <h6 class="text-light">Cerrado</h6>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-white mb-4">Informaciones</h4>
+                <p>Registra tu correo para recibir información</p>
+                <div class="position-relative w-100">
+                    <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Correo">
+                    <button type="button" class="btn btn-light py-2 position-absolute top-0 end-0 mt-2 me-2">Enviar</button>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Footer End -->
+</div>
+<!-- Footer End -->
 
-
-    <!-- Copyright Start -->
-    <div class="container-fluid copyright py-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a class="fw-medium text-light" href="#">FACNET</a>, Todos Los Derechos Reservados.
-                </div>
-                <div class="col-md-6 text-center text-md-end">
-                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                    Diseñado Por <a class="fw-medium text-light" href="">Avocado | Team</a>
-                    Distribuido por <a class="fw-medium text-light" href="">Avocado | Team</a>
-                </div>
+<!-- Copyright Start -->
+<div class="container-fluid copyright py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                &copy; <a class="fw-medium text-light" href="#">FACNET</a>, Todos Los Derechos Reservados.
+            </div>
+            <div class="col-md-6 text-center text-md-end">
+                Diseñado Por <a class="fw-medium text-light" href="">Avocado | Team</a>
+                Distribuido por <a class="fw-medium text-light" href="">Avocado | Team</a>
             </div>
         </div>
     </div>
-    <!-- Copyright End -->
+</div>
+<!-- Copyright End -->
