aaceab6fabcd06b98a32686fb2396db823403282
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-06-07 17:32:59 -0500

    Agregado: sistema de caducacion de token por tiempo

--- php/sesion_validacion.php
+++ php/sesion_validacion.php
@@ -1,9 +1,31 @@
 <?php
     // Verificar si el token de autenticación está presente en el Local Storage
     echo "<script>
-            var token = localStorage.getItem('auth_token');
-            if (!token) {
-              window.location.href = '../login.php';
+            var tiempoInactividad = 1 * 60 * 1000;
+            var inactividad;
+        
+            function iniciarTemporizador() {
+            inactividad = setTimeout(function() {
+                localStorage.removeItem('auth_token');
+                window.location.href = '../login.php';
+                console.log('Redireccionando al usuario');
+            }, tiempoInactividad);
             }
+        
+            function reiniciarTemporizador() {
+            clearTimeout(inactividad);
+            iniciarTemporizador();
+            }
+        
+            var token = localStorage.getItem('auth_token');
+        
+            if (token) {
+            iniciarTemporizador();
+            } else {
+            window.location.href = '../login.php';
+            console.log('Redireccionando al usuario');
+            }
+        
+            document.addEventListener('mousemove', reiniciarTemporizador);
           </script>";
 ?>
