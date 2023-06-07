710619794b689bfcdcd6ff590c47c592ba82cba9
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-06-07 17:05:27 -0500

    Optimización de eventos y correcciones de seguridad

--- eventos.php
+++ eventos.php
@@ -38,8 +38,10 @@
 </head>
 
 <body>
-     <?php require("cabecera.php");  ?>
-<!-- Navbar End -->
+    <?php require_once("cabecera.php"); ?>
+
+    <!-- Navbar End -->
+
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container text-center py-5">
@@ -56,71 +58,86 @@
     <!-- Page Header End -->
 
     <div style="margin-left:100px; margin-right:100px; ">
-          <div class="container-fluid">
-        <!-- Seccion de cursos -->
-        <div class="jumbotron">
-        <h1 class="display-6 mb-4">Eventos Disponibles</h1> 
-        <br>
-        <br>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-          <!-- Codigo de la base de datos -->
-          <?php
-               require'conexion.php';
-               $conexion=conectar();
-                $consulta = "SELECT*FROM `eventos`;";
-                $resultado=mysqli_query($conexion,$consulta);;
-                
-                foreach($resultado as $row){ $id = $row["id_evento"]; ?>
-          <!-- Fin bd -->
+        <div class="container-fluid">
+            <!-- Seccion de cursos -->
+            <div class="jumbotron">
+                <h1 class="display-6 mb-4">Eventos Disponibles</h1>
+                <br>
+                <br>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <!-- Codigo de la base de datos -->
+                    <?php
+                    require_once("conexion.php");
+                    $conexion = conectar();
 
-          <?php
-          if(isset($_SESSION['msj'])) {
-            $respuesta = $_SESSION['msj']; ?>
-          <script>
-               alert('hola');
-          </script>
-          <?php
-        }
-          ?>
-            <!-- Estructura de eventos -->
-                        <div class="card">
-                        <div class="card-body">
-                  <div class="project-item mb-5">
-                    <div class="position-relative">
-                        <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen"]); ?>" class="img-fluid">               
-                        <div class="project-overlay">
-                            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="data:image/jpg;base64,<?php echo base64_encode($row["imagen"]); ?>"
-                                data-lightbox="project"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="eventos.php"><i class="fa fa-link"></i></a>
-                        </div>
-                    </div>
-                     </div>
-                            <h5 class="card-title"><?php echo $row["nombreEven"]; ?></h5>
-                            <p class="card-text"><?php echo $row["descripcionEven"]; ?></p>
-                            <p class="card-text"><small class="text-muted">Lugar: <?php echo $row["lugar"]; ?></small></p>
-                            <p class="card-text"><small class="text-muted">Fecha: <?php echo $row["fecha"]; ?></small></p>
-                            <p class="card-text"><small class="text-muted">Hora: <?php echo $row["hora"]; ?></small></p>
-                            <p class="card-text"><small class="text-muted">Estado del Evento: <?php echo $row["estado"]; ?></small></p>
-                        </div>
-                        <a style="margin: 2px; border-radius: 5px;" href="inceventos.php?id_evento=<?php echo $row["id_evento"]?>" class="btn btn-primary" id = "asistir" > Asistir A Evento</a>
-                        <a style="margin: 2px; border-radius: 5px;" href="galeriaeventos.php?id_evento=<?php echo $row["id_evento"]?>" class="btn btn-info" > Ver Mas</a>
+                    $consulta = "SELECT * FROM eventos";
+                    $resultado = mysqli_query($conexion, $consulta);
 
-                         <br>
-                        <a href="">ㅤㅤ</a>
-                        </div>
+                    foreach ($resultado as $row) {
+                        $id = $row["id_evento"];
+                        ?>
+                        <!-- Fin bd -->
 
-            <!-- ----------------------------- -->
-            <?php } ?>
-          <!--  Fin estructura de cursos --------------- -->
+                        <?php
+                        if (isset($_SESSION['msj'])) {
+                            $respuesta = $_SESSION['msj'];
+                            ?>
+                            <script>
+                                alert('hola');
+                            </script>
+                        <?php
+                        }
+                        ?>
+
+                        <!-- Estructura de eventos -->
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="project-item mb-5">
+                                        <div class="position-relative">
+                                            <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen"]); ?>"
+                                                class="img-fluid">
+                                            <div class="project-overlay">
+                                                <a class="btn btn-lg-square btn-light rounded-circle m-1"
+                                                    href="data:image/jpg;base64,<?php echo base64_encode($row["imagen"]); ?>"
+                                                    data-lightbox="project"><i class="fa fa-eye"></i></a>
+                                                <a class="btn btn-lg-square btn-light rounded-circle m-1"
+                                                    href="eventos.php"><i class="fa fa-link"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h5 class="card-title"><?php echo htmlspecialchars($row["nombreEven"]); ?></h5>
+                                    <p class="card-text"><?php echo htmlspecialchars($row["descripcionEven"]); ?></p>
+                                    <p class="card-text"><small class="text-muted">Lugar:
+                                            <?php echo htmlspecialchars($row["lugar"]); ?></small></p>
+                                    <p class="card-text"><small class="text-muted">Fecha:
+                                            <?php echo htmlspecialchars($row["fecha"]); ?></small></p>
+                                    <p class="card-text"><small class="text-muted">Hora:
+                                            <?php echo htmlspecialchars($row["hora"]); ?></small></p>
+                                    <p class="card-text"><small class="text-muted">Estado del Evento:
+                                            <?php echo htmlspecialchars($row["estado"]); ?></small></p>
+                                </div>
+                                <a style="margin: 2px; border-radius: 5px;"
+                                    href="inceventos.php?id_evento=<?php echo htmlspecialchars($row["id_evento"]); ?>"
+                                    class="btn btn-primary" id="asistir"> Asistir A Evento</a>
+                                <a style="margin: 2px; border-radius: 5px;"
+                                    href="galeriaeventos.php?id_evento=<?php echo htmlspecialchars($row["id_evento"]); ?>"
+                                    class="btn btn-info"> Ver Mas</a>
+
+                                <br>
+                                <a href="">ㅤㅤ</a>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    <!--  Fin estructura de eventos --------------- -->
+                </div>
+            </div>
         </div>
-      </div>
-      </div>
-</div>
+    </div>
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i
             class="bi bi-arrow-up"></i></a>
-
 
     <!-- JavaScript Libraries -->
     <script src="botones.js"></script>
@@ -131,11 +148,12 @@
     <script src="lib/easing/easing.min.js"></script>
     <script src="lib/waypoints/waypoints.min.js"></script>
     <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-     <?php require("footer.php");  ?>
+    <?php require_once("footer.php"); ?>
     <script src="lib/lightbox/js/lightbox.min.js"></script>
 
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
 </body>
 <script src="sweet/dist/sweetalert2.all.min.js"></script>
+
 </html>
