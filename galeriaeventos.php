261e009b1d403bbec1547c17fe5d7937fbd32811
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-06-07 17:17:51 -0500

    optimización y seguridad en galeria de eventos

--- galeriaeventos.php
+++ galeriaeventos.php
@@ -1,3 +1,22 @@
+<?php
+//cabecera.php
+
+// Conexión a la base de datos
+require 'conexion.php';
+$conexion = conectar();
+
+// Obtener el id_evento desde la URL
+$id_evento = $_GET['id_evento'];
+
+// Consulta para obtener los datos del evento
+$consulta = "SELECT * FROM eventos WHERE id_evento = '" . $id_evento . "'";
+$resultado = mysqli_query($conexion, $consulta);
+
+// Verificar si se encontró el evento
+if (mysqli_num_rows($resultado) > 0) {
+    $row = mysqli_fetch_assoc($resultado);
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -15,9 +34,7 @@
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
@@ -36,191 +53,55 @@
 </head>
 
 <body>
-     <?php require("cabecera.php");  ?>
-<!-- Navbar End -->
+    <!-- Navbar -->
+    <?php require("cabecera.php");  ?>
+    <!-- Navbar End -->
 
-<?php
-               require 'conexion.php';
-               $conexion=conectar();
-               $id_evento=$_GET['id_evento'];
-                $consulta = "SELECT*FROM `eventos` where id_evento='".$id_evento."'";
-                $resultado=mysqli_query($conexion,$consulta);
-
-                
-                foreach($resultado as $row){ ?>
-   
-    <div style="margin-left:100px; margin-right:100px;  ">
-          <div class="container-fluid">
-        <!-- Seccion de cursos -->
-        <div class="jumbotron">
-        <h1 class="display-6 mb-4"><?php echo $row["nombreEven"]; ?> </h1>
-
-        <br>
-        <br>
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-          <!-- Codigo de la base de datos -->
-          
-          <!-- Fin bd -->
-            <!-- Estructura de eventos -->
-                        <div class="card">
+    <div style="margin-left:100px; margin-right:100px;">
+        <div class="container-fluid">
+            <!-- Seccion de cursos -->
+            <div class="jumbotron">
+                <h1 class="display-6 mb-4"><?php echo htmlspecialchars($row["nombreEven"]); ?></h1>
+                <br>
+                <br>
+                <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <!-- Estructura de eventos -->
+                    <div class="card">
                         <div class="card-body">
-                        <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen"]); ?>" class="card-img-top">
-                          <br>
+                            <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen"]); ?>" class="card-img-top">
                             <br>
-                            <p class="card-text"><small class="text-muted">Lugar:ㅤㅤ <?php echo $row["lugar"]; ?></small></p>
-                            <p class="card-text"><small class="text-muted">Fecha:ㅤㅤ <?php echo $row["fecha"]; ?></small></p>
-                            <p class="card-text"><small class="text-muted">Hora:ㅤㅤ <?php echo $row["hora"]; ?></small></p>
+                            <br>
+                            <p class="card-text"><small class="text-muted">Lugar: <?php echo htmlspecialchars($row["lugar"]); ?></small></p>
+                            <p class="card-text"><small class="text-muted">Fecha: <?php echo htmlspecialchars($row["fecha"]); ?></small></p>
+                            <p class="card-text"><small class="text-muted">Hora: <?php echo htmlspecialchars($row["hora"]); ?></small></p>
                         </div>
-                         
-                        <a style="margin: 2px; border-radius: 5px;" href="eventos.php" class="btn btn-info" > Regresar</a>
-                          <br>
+                        <div class="card-footer">
+                            <a href="inscribirse.php?id_evento=<?php echo $row["id_evento"]; ?>" class="btn btn-primary btn-block">Inscribirse</a>
                         </div>
-
-                         
-          <!-- Codigo de la base de datos -->
-          <br>
-          <!-- Fin bd -->
-            <!-- Estructura de eventos -->
-                        <div class="card">
-                        <div class="card-body">
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                        <h1 class="display-6 mb-4">Nombre Del Evento:ㅤㅤ <?php echo $row["nombreEven"]; ?></h1>
-                         <br>
-                            
-                            <p class="card-text">Descripcion Del Evento:ㅤㅤ <?php echo $row["descripcionEven"]; ?></p>
-
-                            <p class="card-text"><small class="text-muted">Lugar:ㅤㅤ <?php echo $row["lugar"]; ?></small></p>
-                            <p class="card-text"><small class="text-muted">Fecha:ㅤㅤ <?php echo $row["fecha"]; ?></small></p>
-                            <p class="card-text"><small class="text-muted">Hora:ㅤㅤ <?php echo $row["hora"]; ?></small></p>
-                             <p class="card-text"><small class="text-muted">Estado Del Evento:ㅤㅤ <?php echo $row["estado"]; ?></small></p>
-
-                        </div>
-                         <br>
-                        <a href="">ㅤㅤ</a>
-                        </div>
-                        
-                         </div>
-                         
-      </div>
-      </div>
-</div>
-<br>
-<br>
-
-            <!-- Gallery -->
-<div style="margin-left:100px; margin-right:100px;  ">
- <h1 class="display-6 mb-4">Galeria de Imagenes</h1>
- <br>
-<br>
-<div class="row">
-  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-       <div class="project-item mb-5">
-    <div class="position-relative">
-    <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen1"]); ?>" class="img-fluid">               
-        <div class="project-overlay">
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="data:image/jpg;base64,<?php echo base64_encode($row["imagen1"]); ?>"
-            data-lightbox="project"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="eventos.php"><i class="fa fa-link"></i></a>
+                    </div>
+                    <!-- Fin de estructura de eventos -->
+                </div>
+            </div>
+            <!-- Fin de seccion de cursos -->
         </div>
     </div>
-                   
-</div>
 
-    <div class="project-item mb-5">
-    <div class="position-relative">
-    <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen2"]); ?>" class="img-fluid">               
-        <div class="project-overlay">
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="data:image/jpg;base64,<?php echo base64_encode($row["imagen2"]); ?>"
-            data-lightbox="project"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="eventos.php"><i class="fa fa-link"></i></a>
-        </div>
-    </div>
-                   
-</div>
-  </div>
-
-  <div class="col-lg-4 mb-4 mb-lg-0">
-   <div class="project-item mb-5">
-    <div class="position-relative">
-    <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen3"]); ?>" class="img-fluid">               
-        <div class="project-overlay">
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="data:image/jpg;base64,<?php echo base64_encode($row["imagen3"]); ?>"
-            data-lightbox="project"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="eventos.php"><i class="fa fa-link"></i></a>
-        </div>
-    </div>
-                   
-</div>
-
-   <div class="project-item mb-5">
-    <div class="position-relative">
-    <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen4"]); ?>" class="img-fluid">               
-        <div class="project-overlay">
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="data:image/jpg;base64,<?php echo base64_encode($row["imagen4"]); ?>"
-            data-lightbox="project"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="eventos.php"><i class="fa fa-link"></i></a>
-        </div>
-    </div>
-                   
-</div>
-  </div>
-
-  <div class="col-lg-4 mb-4 mb-lg-0">
-
-   <div class="project-item mb-5">
-    <div class="position-relative">
-    <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen5"]); ?>" class="img-fluid">               
-        <div class="project-overlay">
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="data:image/jpg;base64,<?php echo base64_encode($row["imagen5"]); ?>"
-            data-lightbox="project"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="eventos.php"><i class="fa fa-link"></i></a>
-        </div>
-    </div>
-                   
-</div>
-
-   <div class="project-item mb-5">
-    <div class="position-relative">
-    <img src="data:image/jpg;base64,<?php echo base64_encode($row["imagen6"]); ?>" class="img-fluid">               
-        <div class="project-overlay">
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="data:image/jpg;base64,<?php echo base64_encode($row["imagen6"]); ?>"
-            data-lightbox="project"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="eventos.php"><i class="fa fa-link"></i></a>
-        </div>
-    </div>
-                   
-</div>
-
-
-
-  </div>
-</div>
-</div>
-<!-- Gallery -->
-            <?php } ?>
-          <!--  Fin estructura de cursos --------------- -->
-       
-
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i
-            class="bi bi-arrow-up"></i></a>
-
-
-    <!-- JavaScript Libraries -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-     <?php require("footer.php");  ?>
-    <script src="lib/lightbox/js/lightbox.min.js"></script>
-
-    <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.5.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 
 </html>
+
+<?php
+} else {
+    echo "No se encontró el evento.";
+}
+
+// Cerrar conexión a la base de datos
+mysqli_close($conexion);
+?>
