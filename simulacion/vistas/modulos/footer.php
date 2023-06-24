342def0f9844ab0159802689799737683dd046a2
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-06-24 08:58:41 -0500

    Desestimado Dashboard 2 eliminación de archivos

--- simulacion/vistas/modulos/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-	
-	<strong>Copyright &copy; 2023 <a href="www.facnet.com" target="_blank">Facnet - Aguacate Explosivo dev Team</a>.</strong>
-
-	Todos los derechos reservados.
-
-
-</footer>
