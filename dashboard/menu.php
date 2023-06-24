ce539de3ce0f1b9facfbd05c62ad826ed4790fae
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-06-24 14:45:29 -0500

    correciones en el menu optimización de carga

--- dashboard/menu.php
+++ dashboard/menu.php
@@ -1,15 +1,35 @@
 <head>
 <!--ESTILOS-->
-<link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-<!--datables CSS básico-->
-<link rel="stylesheet" type="text/css" href="datatables/datatables.min.css"/>
-<link rel="stylesheet"  type="text/css" href="datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="bower_components/metisMenu/dist/metisMenu.min.css">
+<link rel="stylesheet" type="text/css" href="datatables/datatables.min.css">
+<link rel="stylesheet" type="text/css" href="datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-<link href="css/style.css" rel="stylesheet">
-<link href="css/stylese.css" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/stylese.css">
+
 
 <!--SCRIPTS-->
+<link rel="preload" href="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js" as="script">
+<link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" as="script">
+<link rel="preload" href="bower_components/metisMenu/dist/metisMenu.min.js" as="script">
+<link rel="preload" href="js/jquery.nicescroll.js" as="script">
+<link rel="preload" href="js/waves.js" as="script">
+<link rel="preload" href="js/myadmin.js" as="script">
+<link rel="preload" href="popper/popper.min.js" as="script">
+<link rel="preload" href="datatables/datatables.min.js" as="script">
+<link rel="preload" href="datatables/Buttons-1.5.6/js/dataTables.buttons.min.js" as="script">
+<link rel="preload" href="datatables/JSZip-2.5.0/jszip.min.js" as="script">
+<link rel="preload" href="datatables/pdfmake-0.1.36/pdfmake.min.js" as="script">
+<link rel="preload" href="datatables/pdfmake-0.1.36/vfs_fonts.js" as="script">
+<link rel="preload" href="datatables/Buttons-1.5.6/js/buttons.html5.min.js" as="script">
+<link rel="preload" href="../../lib/wow/wow.min.js" as="script">
+<link rel="preload" href="../../lib/easing/easing.min.js" as="script">
+<link rel="preload" href="../../lib/waypoints/waypoints.min.js" as="script">
+<link rel="preload" href="../../lib/owlcarousel/owl.carousel.min.js" as="script">
+<link rel="preload" href="../../lib/lightbox/js/lightbox.min.js" as="script">
+<link rel="preload" href="js/main.js" as="script">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" defer></script>
 <script src="bower_components/metisMenu/dist/metisMenu.min.js" defer></script>
@@ -29,6 +49,7 @@
 <script src="../../lib/owlcarousel/owl.carousel.min.js" defer></script>
 <script src="../../lib/lightbox/js/lightbox.min.js" defer></script>
 <script src="js/main.js" defer></script>
+
 </head>
 
 <?php include_once("../php/sesion_validacion.php"); ?>
