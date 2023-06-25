1435b16ed4314007c87c2b94c73da969ff2d1463
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-06-25 09:53:10 -0500

    adaptación plantilla

--- dashboard/DashUsuarios.php
+++ dashboard/DashUsuarios.php
@@ -11,164 +11,26 @@
 <body>
 <?php require_once ("menu.php"); ?>
 
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
+    <div id="page-wrapper">
       <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Blank Page</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
+        <div class="row bg-title">
+          <div class="col-lg-12">
+            <h4 class="page-title">
+              Bienvenido Administrador, ¡desde aquí puedes gestionar los usuarios!
+            </h4>
+            <ol class="breadcrumb">
+              <li><a href="#">Dashboard</a></li>
             </ol>
           </div>
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
-        </div>
-        <div class="card-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  
-<!--antiguo-->
-<section>
-
-<div class="box">
-
-  <div class="box-body">
-    
-   <table class="table table-bordered table-striped dt-responsive tablas">
-     
-    <thead>
-     
-     <tr>
-       
-       <th style="width:10px">#</th>
-       <th>Nombre</th>
-       <th>Usuario</th>
-       <th>Foto</th>
-       <th>Perfil</th>
-       <th>Estado</th>
-       <th>Último login</th>
-       <th>Acciones</th>
-
-     </tr> 
-
-    </thead>
-
-    <tbody>
-      
-      <tr>
-        <td>1</td>
-        <td>Usuario Administrador</td>
-        <td>admin</td>
-        <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-        <td>Administrador</td>
-        <td><button class="btn btn-success btn-xs">Activado</button></td>
-        <td>2017-12-11 12:05:32</td>
-        <td>
-
-          <div class="btn-group">
-              
-            <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-
-            <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-
-          </div>  
-
-        </td>
-
-      </tr>
-
-       <tr>
-        <td>1</td>
-        <td>Usuario Administrador</td>
-        <td>admin</td>
-        <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-        <td>Administrador</td>
-        <td><button class="btn btn-success btn-xs">Activado</button></td>
-        <td>2017-12-11 12:05:32</td>
-        <td>
-
-          <div class="btn-group">
-              
-            <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-
-            <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-
-          </div>  
-
-        </td>
-
-      </tr>
-
-       <tr>
-        <td>1</td>
-        <td>Usuario Administrador</td>
-        <td>admin</td>
-        <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-        <td>Administrador</td>
-        <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
-        <td>2017-12-11 12:05:32</td>
-        <td>
-
-          <div class="btn-group">
-              
-            <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-
-            <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-
-          </div>  
-
-        </td>
-
-      </tr>
-
-    </tbody>
-
-   </table>
-
-  </div>
-
-</div>
-
-</section>
-            <!-- Fin data Table -->
-            </div>
    
+<!-- inicio-->
+            <div class="boton">
+            <a type="button" href="simulaciones/overview.php" class="btny">Empieza a Simular</a>
+
+          </div>
   </div>
   <?php require_once ("footer.php"); ?>
 </body>
+</html>
 </html>
