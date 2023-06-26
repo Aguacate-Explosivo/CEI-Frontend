c52e19c3facd3ceaab5a2991e474872637985d7d
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-06-25 22:56:26 -0500

    por fin funciona esa gonorrea

--- dashboard/agguser.php
+++ dashboard/agguser.php
@@ -1,4 +1,14 @@
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <title>Procesando Cursos </title>
+</head>
+<body>  
+
 <?php
 require_once('../conexion.php');
 $conexion = conectar();
@@ -37,3 +47,6 @@ if ($stmt = mysqli_prepare($conexion, $consulta)) {
 
 mysqli_close($conexion);
 ?>
+
+</body>
+</html>
