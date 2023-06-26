c57d3d2dd6411b4b5d2b18b49f6411c31848f80b
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-06-26 08:39:45 -0500

    correcion de bug usuarios sigue sin funcionar el editar

--- dashboard/controladorusuario.php
+++ dashboard/controladorusuario.php
@@ -1,3 +1,12 @@
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <title>Procesando Cursos </title>
+</head>
+<body>
 <?php
 require_once('../conexion.php');
 $conexion = conectar();
@@ -145,3 +154,6 @@ if (isset($_POST['id'])) {
 
 mysqli_close($conexion);
 ?>
+
+</body>
+</html>
