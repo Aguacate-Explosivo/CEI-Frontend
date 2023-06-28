4b8cf021f8949461f017f0e5b0961de22758813a
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-06-28 04:37:53 -0500

    agregado modulo de estructura organizacional y logo

---
+++ estructura_logo.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/logo.webp">
+    <title>CEI | Estructura y Logo</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+<!-- Favicon -->
+<link href="img/favicon.webp" rel="icon">
+<!-- Google Web Fonts -->
+<link rel="preload" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap">
+</noscript>
+<!-- Icon Font Stylesheet -->
+<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+</noscript>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" defer>
+<!-- Libraries Stylesheet -->
+<link href="lib/animate/animate.min.css" rel="stylesheet" async>
+<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" async>
+<link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" async>
+<!-- Customized Bootstrap Stylesheet -->
+<link href="css/bootstrap.min.css" rel="stylesheet" async>
+<!-- Template Stylesheet -->
+<link href="css/style.css" rel="stylesheet" async>
+<body>  
+    <?php require("cabecera.php"); ?>
+    <!-- Navbar End -->
+
+    <!-- Page Header Start -->
+    <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container text-center py-5">
+            <h1 class="display-2 text-white mb-4 animated slideInDown">Estructura Organizacional y Logo</h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+                <ol class="breadcrumb justify-content-center mb-0">
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!-- Page Header End -->
+    
+    <!-- info Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <h1 class="display-6 mb-4 text-center">Logo</h1>
+            <div class="row g-0 feature-row">
+                <div class="col-md-12 col-lg-12 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="feature-item border h-100 p-5">
+                        <p class="mb-0">Los colores del logotipo representan a la UTCH y al departamento de Chocó, alusivos la riqueza biogeográfica de nuestra región. La bombilla representa a la iluminación de la creatividad, el pensamiento, la innovación.</p>
+                        <img class="img-fluid mx-auto d-block" src="img\logo.webp" alt="Logo del CEI">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- info End -->
+
+    <!-- Programa Radical -->
+        <div class="container-xxl py-5">
+        <div class="container">
+            <h1 class="display-6 mb-4 text-center">Estructura Organizacional</h1>
+            <div class="row g-0 feature-row">
+                <div class="col-md-12 col-lg-12 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="feature-item border h-100 p-5">
+                        <p class="mb-0"></p>
+                        <img class="img-fluid mx-auto d-block" src="img\estructura_organizacional_CEI.webp" alt="Imagen de la estructura organizacional del CEI">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Programa Radical Fin -->
+
+
+    <?php require("footer.php"); ?>
+    <!-- Footer End -->
+
+    <!-- JavaScript Libraries -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.0/js/bootstrap.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.easing/1.4.1/jquery.easing.min.js" defer></script>
+
+    <!-- Template Javascript -->
+    <script src="lib/wow/wow.min.js" defer></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js" defer></script>
+    <script src="lib/isotope/isotope.pkgd.min.js" defer></script>
+    <script src="lib/lightbox/js/lightbox.min.js" defer></script>
+    <script src="js/main.js" defer></script>
+</body>
+
+</html>
