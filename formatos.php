0a3885869f2f8acacf873ec414950e6fad71c458
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-06-28 04:38:25 -0500

    agregado modulo de formatos en pdf

---
+++ formatos.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/logo.webp">
+    <title>CEI | Descargar Formatos</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+<!-- Favicon -->
+<link href="img/favicon.webp" rel="icon">
+<!-- Google Web Fonts -->
+<link rel="preload" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap">
+</noscript>
+<!-- Icon Font Stylesheet -->
+<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+</noscript>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" defer>
+<!-- Libraries Stylesheet -->
+<link href="lib/animate/animate.min.css" rel="stylesheet" async>
+<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" async>
+<link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" async>
+<!-- Customized Bootstrap Stylesheet -->
+<link href="css/bootstrap.min.css" rel="stylesheet" async>
+<!-- Template Stylesheet -->
+<link href="css/style.css" rel="stylesheet" async>
+<body>  
+    <?php require("cabecera.php"); ?>
+    
+    <!-- info Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <h1 class="display-6 mb-4 text-center">Descargar Formatos</h1>
+            <div class="row g-0 feature-row">
+                <div class="col-md-12 col-lg-12 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="feature-item border h-100 p-5">
+                                                <!-- Enlace 1 -->
+                        <a href="https://www.utch.edu.co/portal/images/acercade/cei/formatos/BASICO_EN_COSTOS_DE_PRODUCCION.pdf" class="d-flex align-items-center p-3">
+                            <img class="img-fluid d-block" src="img\icon\pdf.webp" alt="Logo de los pdf's" style="width:3.5em">
+                            <span class="p-2">Básico en costo de producción</span>
+                        </a>
+                                                <!-- Enlace 2 -->
+                        <a href="https://www.utch.edu.co/portal/images/acercade/cei/formatos/DERECHO_EMPRESARIAL.pdf" class="d-flex align-items-center p-3">
+                            <img class="img-fluid d-block" src="img\icon\pdf.webp" alt="Logo de los pdf's" style="width:3.5em">
+                            <span class="p-2">Derecho empresarial</span>
+                        </a>
+                                                <!-- Enlace 3 -->
+                        <a href="https://www.utch.edu.co/portal/images/acercade/cei/formatos/DIRECCION_DE_MARKETING.pdf" class="d-flex align-items-center p-3">
+                            <img class="img-fluid d-block" src="img\icon\pdf.webp" alt="Logo de los pdf's" style="width:3.5em">
+                            <span class="p-2">Dirección de marketing</span>
+                        </a>
+                                                <!-- Enlace 4 -->
+                        <a href="https://www.utch.edu.co/portal/images/acercade/cei/formatos/EVALUACION_DE_MODELOS_DE_NEGOCIOS_BASADAOS_EN_EL_LIENZO_CANVAS.pdf" class="d-flex align-items-center p-3">
+                            <img class="img-fluid d-block" src="img\icon\pdf.webp" alt="Logo de los pdf's" style="width:3.5em">
+                            <span class="p-2">Evaluación de modelos de negocios basados en el lienzo Canvas</span>
+                        </a>
+                                                <!-- Enlace 5 -->
+                        <a href="https://www.utch.edu.co/portal/images/acercade/cei/formatos/FUNDAMENTACION_PARA_EL_EMPRENDIMIENTO.pdf" class="d-flex align-items-center p-3">
+                            <img class="img-fluid d-block" src="img\icon\pdf.webp" alt="Logo de los pdf's" style="width:3.5em">
+                            <span class="p-2">Fundamentación para el emprendimiento</span>
+                        </a>
+                                                <!-- Enlace 6 -->
+                        <a href="https://www.utch.edu.co/portal/images/acercade/cei/formatos/IDENTIFICACION_Y_VALORACION_DE_INICIATIVAS_DE_NEGOCIOS.pdf" class="d-flex align-items-center p-3">
+                            <img class="img-fluid d-block" src="img\icon\pdf.webp" alt="Logo de los pdf's" style="width:3.5em">
+                            <span class="p-2">Identificación y valoración de iniciativas de negocios</span>
+                        </a>
+                                                <!-- Enlace 7 -->
+                        <a href="https://www.utch.edu.co/portal/images/acercade/cei/formatos/INDUCCION_Y_BASICO_DE_ECONOMIA_SOLIDARIA.pdf" class="d-flex align-items-center p-3">
+                            <img class="img-fluid d-block" src="img\icon\pdf.webp" alt="Logo de los pdf's" style="width:3.5em">
+                            <span class="p-2"> Inducción y básico de económia solidaria</span>
+                        </a>
+                                                <!-- Enlace 8 -->
+                        <a href="https://www.utch.edu.co/portal/images/acercade/cei/formatos/MANEJO_DE_SIMULADORES_FINANCIEROS.pdf" class="d-flex align-items-center p-3">
+                            <img class="img-fluid d-block" src="img\icon\pdf.webp" alt="Logo de los pdf's" style="width:3.5em">
+                            <span class="p-2">Manejo de simuladores financieros</span>
+                        </a>
+                                                <!-- Enlace 9 -->
+                        <a href="https://www.utch.edu.co/portal/images/acercade/cei/formatos/TECNICAS_DE_MUESTREO_Y%20DISENO_DE_CUESTIONARIOS.pdf" class="d-flex align-items-center p-3">
+                            <img class="img-fluid d-block" src="img\icon\pdf.webp" alt="Logo de los pdf's" style="width:3.5em">
+                            <span class="p-2">Técnicas de muestreo y diselo de cuestionarios</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- info End -->
+
+    <?php require("footer.php"); ?>
+    <!-- Footer End -->
+
+    <!-- JavaScript Libraries -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.0/js/bootstrap.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.easing/1.4.1/jquery.easing.min.js" defer></script>
+
+    <!-- Template Javascript -->
+    <script src="lib/wow/wow.min.js" defer></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js" defer></script>
+    <script src="lib/isotope/isotope.pkgd.min.js" defer></script>
+    <script src="lib/lightbox/js/lightbox.min.js" defer></script>
+    <script src="js/main.js" defer></script>
+</body>
+
+</html>
