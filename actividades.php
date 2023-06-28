74901fcd207c17b065ac3fbb640921ebfc392c7d
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-06-28 04:37:28 -0500

    agregado modulo de actividades realizadas

---
+++ actividades.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/logo.webp">
+    <title>CEI | Actividades Realizadas</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+<!-- Favicon -->
+<link href="img/favicon.webp" rel="icon">
+<!-- Google Web Fonts -->
+<link rel="preload" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap">
+</noscript>
+<!-- Icon Font Stylesheet -->
+<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+</noscript>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" defer>
+<!-- Libraries Stylesheet -->
+<link href="lib/animate/animate.min.css" rel="stylesheet" async>
+<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" async>
+<link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" async>
+<!-- Customized Bootstrap Stylesheet -->
+<link href="css/bootstrap.min.css" rel="stylesheet" async>
+<!-- Template Stylesheet -->
+<link href="css/style.css" rel="stylesheet" async>
+<body>  
+    <?php require("cabecera.php"); ?>
+    <!-- Navbar End -->
+
+    <!-- Page Header Start -->
+    <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container text-center py-5">
+            <h1 class="display-2 text-white mb-4 animated slideInDown">Actividades Realizadas</h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+                <ol class="breadcrumb justify-content-center mb-0">
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!-- Page Header End -->
+    
+    <!-- info Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <h1 class="display-6 mb-4 text-center">Jornada de Sensibilización Estudiantes</h1>
+            <div class="row g-0 feature-row">
+                <div class="col-md-12 col-lg-12 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="feature-item border h-100 p-5">
+                        <p class="mb-0">Es una estrategia de acercamiento inicial a estudiantes y docentes de la universidad tecnológica del chocó a través de la cual se les da a conocer las políticas y acuerdos que sustentan el quehacer entorno al emprendimiento adoptado por la universidad y liderado por el centro, así como los servicios y ofertas de cursos cortos, horarios de atención y disponibilidad del personal docente para acompañamiento y asesoría.<br>1. Acercamiento a los salones previamente identificados para dar a conocer lo siguiente y procesos de sensibilización:</p>
+                        <ul>
+                            <li>Quienes somos: El CEIUTCH, es una unidad administrativa de servicios, adscrita a la Rectoría  de la UTCH</li>
+                            <li>
+                            Que hacemos: El CEIUTCH, se encarga de ofrecer a todos los estudiantes, egresados, docentes, administrativos y comunidad en general. Programas de:
+                                <ol>
+                                    <li>Formación y Capacitación Empresarial</li>
+                                    <li>Asesoramiento y acompañamiento en proyectos empresariales</li>
+                                    <li>Asesoramiento en presentación a convocatorias para capital semilla</li>
+                                    <li>Gestión y desarrollo empresarial.</li>
+                                    <li>Investigación (semillero)</li>
+                                    <li>Búsqueda de Financiación para proyectos.</li>
+                                    <li>Espacio de interacción y sinergia entre emprendedores de la región (Club de emprendedores, red de emprendimiento) </li>
+                                </ol>
+                            </li>
+                            <li>Donde estamos: El CEIUTCH, está ubicada en la ciudadela Universitaria, bloque 8, piso 3, oficinas 316 y 318</li>
+                            <li>Que buscamos:  El CEIUTCH, busca; Generar conocimiento sobre emprendimiento e Incentivar el emprendimiento como un estilo de vida  Apoyar a los emprendedores en el desarrollo de sus iniciativas y proyectos empresariales</li>
+                            <li>Fomentar la sostenibilidad de nuevas empresas</li>
+                            <li>Equipo de trabajo: Contamos con un equipo multidisciplinario, integrado por 12 profesionales en distintas especialidades, para el asesoramiento integral en todas las áreas funcionales de la empresa y en todas las etapas de un proyecto.</li>
+                            <li>Horario de atención: lunes a viernes de 8:00 am. A 12:00 m. y de 2:00 pm. A 6:00 pm.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- info End -->
+
+    <!-- Programa Radical -->
+        <div class="container-xxl py-5">
+        <div class="container">
+            <h1 class="display-6 mb-4 text-center">Programa radial</h1>
+            <div class="row g-0 feature-row">
+                <div class="col-md-12 col-lg-12 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="feature-item border h-100 p-5">
+                        <p class="mb-0">La hora del emprendimiento es un programa radial que esta al aire todos los jueves de 10:00–11:00 am por la emisora radio universidad del Choco 97.3 FM, y nace por iniciativa del centro de emprendimiento e innovación de la Universidad Tecnológica del Choco “Diego Luis Córdoba” con el propósito de promover e incentivar la cultura de emprendimiento en la comunidad en general y destacar actividades y eventos promovidos por emprendedores de la región.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Programa Radical Fin -->
+
+
+    <?php require("footer.php"); ?>
+    <!-- Footer End -->
+
+    <!-- JavaScript Libraries -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.0/js/bootstrap.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.easing/1.4.1/jquery.easing.min.js" defer></script>
+
+    <!-- Template Javascript -->
+    <script src="lib/wow/wow.min.js" defer></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js" defer></script>
+    <script src="lib/isotope/isotope.pkgd.min.js" defer></script>
+    <script src="lib/lightbox/js/lightbox.min.js" defer></script>
+    <script src="js/main.js" defer></script>
+</body>
+
+</html>
