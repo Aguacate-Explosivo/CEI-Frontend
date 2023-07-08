c5cee2caee94676b0ee0a1dc6ff41a56f8e5c79e
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-07-08 10:52:10 -0500

    adaptación encuestas maqueta

--- encuesta.php
+++ encuesta.php
@@ -21,51 +21,12 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <h1 class="display-5 mb-5">Agenda Tu cita</h1>
+                <h1 class="display-5 mb-5">ENCUESTA</h1>
             </div>
-            <div class="row g-5">
+            <div class="row">
                 <div class="col-lg-6 mx-auto wow fadeInUp" data-wow-delay="0.1s">
-                    <h3 class="mb-4">Formulario para agendamiento de Citas "CEI"</h3>
-                    <p class="mb-4">¡Wow, tenemos una gran noticia para ti! Si deseas programar una cita para poder visitar nuestra oficina y ser atendido por nuestro equipo de profesionales, aquí te damos la solución que necesitas. Simplemente, completa el formulario que hemos preparado para poder separar una cita dentro de nuestros horarios de atención. Así, podrás acercarte a nuestras instalaciones con toda la confianza de ser atendido con eficacia y eficiencia por nuestro equipo experto. ¡No esperes más y programa tu cita ahora mismo!</p>
-                    <form action="php/reg_cita.php" method="POST">
-                        <div class="row g-3">
-                            <div class="col-md-6">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" id="documento" name="documento" placeholder="Your Name" required pattern="[0-9]+" maxlength="10" minlength="7">
-                                    <label for="documento">Numero de Documento</label>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Your Email" required pattern="[a-zA-ZÁÉÍÓÚáéíóúñ ]+">
-                                    <label for="nombre">Tu Nombre</label>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" id="tel" name="tel" placeholder="Your Name" required pattern="[0-9]+"  maxlength="10" minlength="9">
-                                    <label for="tel">Telefono</label>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-floating">
-                                    <input type="datetime-local" class="form-control" id="fecha" name="fecha" placeholder="Your Email" min="2023-03-01 " max="2030-12-31" step="1800" required >
-                                    <label for="fecha">Fecha y Hora</label>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" id="direccion" name="direccion" required pattern="[a-zA-ZÁÉÍÓÚáéíóúñ 0-9-#]+" placeholder="Subject">
-                                    <label for="direccion">Dirección</label>
-                                </div>
-                            </div>
-                            <br>
-                            <p>Nota: Debes Registar Tu Cita Con Intervalos De 30 Minutos</p>
-                            <div class="col-12">
-                                <button class="btn btn-primary rounded-pill py-3 px-5" type="submit" name="agendar" >Agendar cita</button>
-                            </div>
-                        </div>
-                    </form>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeE9iwdniEh98T7biMgyqZ-Ll0s7wbC49LZoEoahusrACTpUA/viewform?embedded=true" width="700" height="1335" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+                    
                 </div>
             </div>
         </div>
