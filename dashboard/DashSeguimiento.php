ace5a7f969c252fe7257f5c1336fb34570c5399d
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-07-17 06:25:53 -0500

    Create DashSeguimiento.php

---
+++ dashboard/DashSeguimiento.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dashboard Ficha de Seguimiento</title>
+  <link rel="icon" type="image/webp" sizes="16x16" href="../img/logo.webp">
+</head>
+<?php require_once ("menu.php"); ?>
+<body class="hold-transition sidebar-mini">
+    <div id="page-wrapper">
+      <div class="container-fluid">
+        <div class="row bg-title">
+          <div class="col-lg-12">
+            <h4 class="page-title">
+              Desde aquí puedes gestionar las fichas de seguimientos a los emprendedores
+            </h4>
+            <ol class="breadcrumb">
+              <li><a href="#">Fichas de Seguimiento</a></li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      <!-- Contenido -->
+      <div class="row">
+          <div class="col-md-12">
+            <div class="white-box">
+              <h3 class="text-center">Fichas de Seguimiento</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <?php require_once ("footer.php"); ?>
+</body>
+</html>
