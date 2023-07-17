5f7817818855cf334b0a1d55c21ef00b84ff777a
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-07-17 06:25:51 -0500

    Update DashSimulaciones.php

--- dashboard/DashSimulaciones.php
+++ dashboard/DashSimulaciones.php
@@ -4,8 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Dashboard Admin</title>
-  <title>Dashboard CEI</title>
+  <title>Dashboard Simulaciones</title>
   <link rel="icon" type="image/webp" sizes="16x16" href="../img/logo.webp">
   <link rel="stylesheet" href="css/stylese.css">
 </head>
