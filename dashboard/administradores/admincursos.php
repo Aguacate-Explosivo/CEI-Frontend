8bb6c2b84cca83a1347065cc6c80d5663cd1b611
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-07-18 09:13:35 -0500

    correción menu paginas restantes

--- dashboard/administradores/admincursos.php
+++ dashboard/administradores/admincursos.php
@@ -44,6 +44,7 @@
 
   <body>
       <?php
+      require_once("menu.php");
       include_once("../sesion_validacion.php")
       ?>
     <!-- Preloader -->
@@ -52,109 +53,7 @@
     </div>
     <div id="wrapper">
       <!-- Navigation -->
-      <nav
-        class="navbar navbar-default navbar-static-top"
-        style="margin-bottom: 0"
-      >
-        <div class="navbar-header">
-          <a
-            class="navbar-toggle hidden-sm hidden-md hidden-lg"
-            href="javascript:void(0)"
-            data-toggle="collapse"
-            data-target=".navbar-collapse"
-            ><i class="ti-menu"></i
-          ></a>
-          <div class="top-left-part">
-            <a class="logo" href="../index.php"
-              ><i class="glyphicon glyphicon-fire"></i>&nbsp;<span
-                class="hidden-xs"
-                >Cursos</span
-              ></a
-            >
-          </div>
-          <ul class="nav navbar-top-links navbar-left hidden-xs">
-            <li>
-              <a
-                href="javascript:void(0)"
-                class="open-close hidden-xs hidden-lg waves-effect waves-light"
-                ><i class="ti-arrow-circle-left ti-menu"></i
-              ></a>
-            </li>
-          </ul>
-          <ul class="nav navbar-top-links navbar-right pull-right">
-            <!-- Buscador -->
 
-            <li>
-              <a class="profile-pic" href="#">
-
-                <i class="ti-user"></i>
-                <b class="hidden-xs">Administrador</b>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-      </nav>
-      <div class="navbar-default sidebar nicescroll" role="navigation">
-        <div class="sidebar-nav navbar-collapse">
-          <ul class="nav" id="side-menu">
-            <li class="sidebar-search hidden-sm hidden-md hidden-lg">
-              <div class="input-group custom-search-form">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Search..."
-                />
-                <span class="input-group-btn">
-                  <button class="btn btn-default" type="button">
-                    <i class="ti-search"></i>
-                  </button>
-                </span>
-              </div>
-            </li>
-            <li>
-              <a href="../index.php" class="waves-effect"
-                ><i class="glyphicon glyphicon-fire fa-fw"></i> Dashboard</a
-              >
-            </li>
-            <li>
-              <a href="../DashCitas.php" class="waves-effect"
-                ><i class="ti-calendar fa-fw"></i>Citas</a
-              >
-            </li>
-            <li>
-              <a href="../DashEventos.php" class="waves-effect"
-                ><i class="ti-video-camera fa-fw"></i>Eventos</a
-              >
-            </li>
-            <li>
-              <a href="../DashCursos.php" class="waves-effect active"
-                ><i class="ti-ink-pen fa-fw"></i> Cursos</a
-              >
-            </li>
-            <li>
-            <a href="../simulaciones\index.php" class="waves-effect ">
-              <i class="ti-stats-up fa-fw"></i>
-             Simulaciones
-            </a>
-          </li>
-          </ul>
-
-          <div class="center p-20">
-            <span class="hide-menu"
-              ><a
-                href="../../php/log_out.php"
-                class="
-                  btn btn-success btn-block btn-rounded
-                  waves-effect waves-light
-                "
-                >Cerra sesión</a
-              ></span
-            >
-          </div>
-        </div>
-        <!-- /.sidebar-collapse -->
-      </div>
       <!-- Page Content -->
       <div id="page-wrapper">
         <div class="container-fluid">
