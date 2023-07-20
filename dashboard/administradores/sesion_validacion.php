410ce43468489edfed6296dc29428d1c3a06f6d7
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-07-20 16:53:32 -0500

    Update sesion_validacion.php

--- dashboard/administradores/sesion_validacion.php
+++ dashboard/administradores/sesion_validacion.php
@@ -3,6 +3,7 @@
     // Verificar si el token de autenticación está presente en el Local Storage
     echo "<script>
             var token = localStorage.getItem('auth_token');
+            localStorage.removeItem('auth_perfil');
             if (!token) {
               window.location.href = '../login.php';
             }
