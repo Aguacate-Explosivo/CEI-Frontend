edc235b55ad8991fccdb9dfdcda2d71468b3b428
Emmadelic Space <32521615+Emagf94@users.noreply.github.com>
2023-07-20 17:22:18 -0500

    hot fix bugs y rutas

--- dashboard/administradores/encacurso.php
+++ dashboard/administradores/encacurso.php
@@ -14,178 +14,19 @@
       content="My Admin is powerful and clean admin dashboard template, inpired from Bootstrap Framework"
     />
     <meta name="robots" content="noindex,nofollow" />
-    <title>Dashboard Cursos</title>
-    <link
-      rel="canonical"
-      href="https://www.wrappixel.com/templates/myadmin-lite/"
-    />
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon.png" />
-    <!-- Bootstrap Core CSS -->
-    <link
-      href="../bower_components/bootstrap/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <!-- Menu CSS -->
-    <link
-      href="../bower_components/metisMenu/dist/metisMenu.min.css"
-      rel="stylesheet"
-    />
-    <!-- Custom CSS -->
-    <link href="../css/style.css" rel="stylesheet" />
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    
-    <!--datables CSS básico-->
-    <link rel="stylesheet" type="text/css" href="../datatables/datatables.min.css"/>
-    <!--datables estilo bootstrap 4 CSS-->  
-    <link rel="stylesheet"  type="text/css" href="../datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
-    <!--font awesome con CDN-->  
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">  
+    <title>Dashboard Cursos</title>  
   </head>
 
   <body>
   <?php
-      include_once("../sesion_validacion.php")
+      require_once("menu.php");
+      include_once("../sesion_validacion.php");
       ?>
     <!-- Preloader -->
     <div class="preloader">
       <div class="cssload-speeding-wheel"></div>
     </div>
-    <div id="wrapper">
-      <!-- Navigation -->
-      <nav
-        class="navbar navbar-default navbar-static-top"
-        style="margin-bottom: 0"
-      >
-        <div class="navbar-header">
-          <a
-            class="navbar-toggle hidden-sm hidden-md hidden-lg"
-            href="javascript:void(0)"
-            data-toggle="collapse"
-            data-target=".navbar-collapse"
-            ><i class="ti-menu"></i
-          ></a>
-          <div class="top-left-part">
-            <a class="logo" href="../index.php"
-              ><i class="glyphicon glyphicon-fire"></i>&nbsp;<span
-                class="hidden-xs"
-                >Cursos</span
-              ></a
-            >
-          </div>
-          <ul class="nav navbar-top-links navbar-left hidden-xs">
-            <li>
-              <a
-                href="javascript:void(0)"
-                class="open-close hidden-xs hidden-lg waves-effect waves-light"
-                ><i class="ti-arrow-circle-left ti-menu"></i
-              ></a>
-            </li>
-          </ul>
-          <ul class="nav navbar-top-links navbar-right pull-right">
-            <!-- Buscador -->
-            <!-- <li>
-              <form role="search" class="app-search hidden-xs">
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  class="form-control"
-                />
-                <a href=""><i class="ti-search"></i></a>
-              </form>
-            </li> -->
-            <li>
-              <a class="profile-pic" href="#">
-                <!-- <img
-                  src="images/users/hritik.jpg"
-                  alt="user-img"
-                  width="36"
-                  class="img-circle"
-                /> -->
-                <i class="ti-user"></i>
-                <b class="hidden-xs">Administrador</b>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <!-- /.navbar-header -->
-        <!-- /.navbar-top-links -->
-        <!-- /.navbar-static-side -->
-      </nav>
-      <div class="navbar-default sidebar nicescroll" role="navigation">
-        <div class="sidebar-nav navbar-collapse">
-          <ul class="nav" id="side-menu">
-            <li class="sidebar-search hidden-sm hidden-md hidden-lg">
-              <div class="input-group custom-search-form">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Search..."
-                />
-                <span class="input-group-btn">
-                  <button class="btn btn-default" type="button">
-                    <i class="ti-search"></i>
-                  </button>
-                </span>
-              </div>
-            </li>
-            <li>
-              <a href="../index.php" class="waves-effect"
-                ><i class="glyphicon glyphicon-fire fa-fw"></i> Dashboard</a
-              >
-            </li>
-            <li>
-              <a href="../DashCitas.php" class="waves-effect"
-                ><i class="ti-calendar fa-fw"></i>Citas</a
-              >
-            </li>
-            <li>
-              <a href="../DashEventos.php" class="waves-effect"
-                ><i class="ti-video-camera fa-fw"></i>Eventos</a
-              >
-            </li>
-            <li>
-              <a href="../DashCursos.php" class="waves-effect active"
-                ><i class="ti-ink-pen fa-fw"></i> Cursos</a
-              >
-            </li>
-            <li>
-            <a href="../simulaciones\index.php" class="waves-effect ">
-              <i class="ti-stats-up fa-fw"></i>
-             Simulaciones
-            </a>
-          </li>
-            <!-- <li>
-              <a href="blank.html" class="waves-effect"
-                ><i class="ti-files fa-fw"></i> Dashboard</a
-              >
-            </li> -->
-            <!-- <li>
-              <a href="404.html" class="waves-effect"
-                ><i class="ti-info fa-fw"></i> Error 404</a
-              >
-            </li> -->
-          </ul>
-          <div class="center p-20">
-            <span class="hide-menu"
-              ><a
-                href="../../php/log_out.php"
-                class="
-                  btn btn-success btn-block btn-rounded
-                  waves-effect waves-light
-                "
-                >Cerra sesión</a
-              ></span
-            >
-          </div>
-        </div>
-        <!-- /.sidebar-collapse -->
-      </div>
+    
       <!-- Page Content -->
       <div id="page-wrapper">
         <div class="container-fluid">
